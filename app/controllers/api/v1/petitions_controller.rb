13a0510b9479356f6c364b5ba22761b0f6a84b9d
Gary Meliman <gary.meliman@outlook.com>
2016-05-19 21:16:11 -0400

    bug fixed

--- app/controllers/api/v1/petitions_controller.rb
+++ app/controllers/api/v1/petitions_controller.rb
@@ -1,5 +1,5 @@
 class API::V1::PetitionsController < Api::V1::BaseController
-  before_action :authenticate_user!
+  #before_action :authenticate_user!
   
   def index
     @petitions = current_user.first.petitions
