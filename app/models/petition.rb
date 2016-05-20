679ec6076c7a662f872ff1a1c64e2eafac12b82d
Gary Meliman <gary.meliman@outlook.com>
2016-05-19 21:05:38 -0400

    petition status to false before_create

--- app/models/petition.rb
+++ app/models/petition.rb
@@ -2,6 +2,10 @@ class Petition < ActiveRecord::Base
   belongs_to :user
   belongs_to :pharmacy
   belongs_to :medicine
+ 
+  before_create do
+    self.status = false
+  end
 
   validates :medicine_id, uniqueness: { scope: :user_id }
 end
